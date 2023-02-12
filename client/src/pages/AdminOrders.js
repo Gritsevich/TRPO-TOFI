05b2ba31f617f2f17d99d1001a2ac338f50a6a8d
Gritsevich <gritsevich.rina@gmail.com>
2023-02-12 11:19:34 +0300

    first commit

---
+++ client/src/pages/AdminOrders.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import { adminGetAll as getAllOrders } from '../http/orderAPI.js'
+import { Button, Container, Spinner } from 'react-bootstrap'
+import Orders from '../components/Orders.js'
+import CreateOrder from '../components/CreateOrder.js'
+
+const AdminOrders = () => {
+    const [orders, setOrders] = useState(null)
+    const [fetching, setFetching] = useState(true)
+    const [show, setShow] = useState(false)
+
+    useEffect(() => {
+        getAllOrders()
+            .then(
+                data => setOrders(data)
+            )
+            .finally(
+                () => setFetching(false)
+            )
+    }, [])
+
+    if (fetching) {
+        return <Spinner animation="border" />
+    }
+
+    return (
+        <Container>
+            <h1>Все заказы</h1>
+            <CreateOrder show={show} setShow={setShow} />
+            <Orders items={orders} admin={true} />
+        </Container>
+    )
+}
+
+export default AdminOrders
---
+++ client/src/pages/Delivery.js
@@ -0,0 +1,15 @@
+import Container from 'react-bootstrap/Container'
+
+const Delivery = () => {
+    return (
+        <Container>
+            <h1>Условия доставки</h1>
+            <p>Срок доставки с момента оформления заказа составляет 3–5 дней
+            <br></br><br></br>
+                Если у Вас есть вопросы по доставке Вашего заказа, свяжитесь с нами по электронной почте: sales@marketplace.by или напрямую Екатерине Тимофеевне
+            </p>
+        </Container>
+    )
+}
+
+export default Delivery
